5463303d3c4f01282fa1f60f37bc296ce7230b24
maferpinazocode <114931982+maferpinazocode@users.noreply.github.com>
2024-10-18 21:46:40 -0500

    We created the WelcomeFragment and its layout fragment_welcome, defined the navigation.xml in the navigation directory to control the destionation, and configured the button to start the game.

---
+++ AppParcial/app/src/main/java/com/example/questionsanswers/WelcomeFragment.kt
@@ -0,0 +1,17 @@
+package com.example.questionsanswers
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class WelcomeFragment : Fragment(R.layout.fragment_welcome) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Configurar botón para empezar el juego
+        view.findViewById<Button>(R.id.startButton).setOnClickListener {
+            findNavController().navigate(R.id.action_welcomeFragment_to_questionFragment)
+        }
+    }
+}
